b8a653a9db59fa82ed640930c085f67a6de67ed4
1 <martin@pixelpudu.com>
2017-06-30 15:38:48 +0100

    fix

--- template-parts/partials/partial-side.php
+++ template-parts/partials/partial-side.php
@@ -17,11 +17,18 @@
 				</div>
 			
 				<h2>SPECIAL OFFER</h2>
-				<?php if (get_field( 'Special_Offer', 'option' )): ?>
-					<a href="<?php the_field( 'Special_Offer_Link', 'option' ); ?>">
-						<?php $advert = get_field( 'Special_Offer', 'option' ); ?>
-						<img src="<?php echo $advert['url']; ?>">
-					</a>
-				<?php endif; ?>
-				
+				<?php
+				if( have_rows('special_offers', 'option') ):
+				    while ( have_rows('special_offers', 'option') ) : the_row();
+
+						if (get_field( 'Special_Offer', 'option' )): ?>
+						<a href="<?php the_field( 'Special_Offer_Link', 'option' ); ?>">
+							<?php $advert = get_field( 'Special_Offer', 'option' ); ?>
+							<img src="<?php echo $advert['url']; ?>">
+						</a>
+						<?php endif;
+
+				    endwhile;
+				endif;
+				?>				
 			</div>
